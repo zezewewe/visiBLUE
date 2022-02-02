823e8bba1954ef440a27cc4ef32de56f16df52e0
zezewewe <zng033@e.ntu.edu.sg>
2022-02-02 10:51:15 +0000

    modify As726 find out what are the values

--- As726x.py
+++ As726x.py
@@ -20,7 +20,6 @@ max_graph = 80
 def graph_map(x):
     return min(int(x * max_graph / max_val), max_graph)
 
-
 # for I2C use:
 i2c = board.I2C()
 sensor = AS726x_I2C(i2c)
@@ -32,13 +31,16 @@ while True:
     while not sensor.data_ready:
         time.sleep(0.1)
 
+    lightValues = [sensor.violet, sensor.blue, sensor.green, sensor.yellow, sensor.orange, sensor.red]
+    print(lightValues, '/n')
+    
     # plot plot the data
-    print("\n")
-    print("V: " + str(graph_map(sensor.violet))) # * "=")
-    print("B: " + str(graph_map(sensor.blue))) # * "=")
-    print("G: " + str(graph_map(sensor.green))) # * "=")
-    print("Y: " + str(graph_map(sensor.yellow))) # * "=")
-    print("O: " + str(graph_map(sensor.orange))) # * "=")
-    print("R: " + str(graph_map(sensor.red))) # * "=")
+    # print("\n")
+    # print("V: " + str(graph_map(sensor.violet))) # * "=")
+    # print("B: " + str(graph_map(sensor.blue))) # * "=")
+    # print("G: " + str(graph_map(sensor.green))) # * "=")
+    # print("Y: " + str(graph_map(sensor.yellow))) # * "=")
+    # print("O: " + str(graph_map(sensor.orange))) # * "=")
+    # print("R: " + str(graph_map(sensor.red))) # * "=")
 
     time.sleep(1)

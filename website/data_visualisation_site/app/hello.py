c804299d216440daa8c7a1063aebc6819841f3e3
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2022-02-24 11:33:19 +0000

    Added recommendations and stats to website

--- website/data_visualisation_site/app/hello.py
+++ website/data_visualisation_site/app/hello.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 def prep_data(time_range_in_mins, include_bluelight_flag, include_ambientlight_flag, user):
    # Set threshold for displaying
    if user == 0:
+      Overallthreshold = 0.9
       HEVthreshold = 0.4
    elif user == 1:
+      Overallthreshold = 0.9
       HEVthreshold = 0.2
 
    # Get current directory
@@ -30,24 +32,47 @@ def prep_data(time_range_in_mins, include_bluelight_flag, include_ambientlight_f
    df[['harmfulHEVIntensity', 'overallLightIntensity', 'artificialLightBool']] = df[['harmfulHEVIntensity', 'overallLightIntensity', 'artificialLightBool']].apply(pd.to_numeric)
    
    # Filter down by appropriate time length
+   time_of_last_collection = []
    time_of_last_collection = df['timeNow'].iloc[-1]
    df = df.loc[((time_of_last_collection - df.timeNow).dt.total_seconds()/60) < time_range_in_mins].copy()
    
 
    # Generate list data
-   data = []
+   data = {}
+   labels = {}
    harmfulHEVIntensity_data = list(df['harmfulHEVIntensity'])
    overallLightIntensity_data = list(df['overallLightIntensity'])
-   data.append(harmfulHEVIntensity_data)
-   data.append(overallLightIntensity_data)
-   data.append([HEVthreshold for x in harmfulHEVIntensity_data])
-   labels = list(df['timeNow'].dt.strftime('%Y-%m-%d, %H:%M'))
+   data['HEV_values'] = harmfulHEVIntensity_data
+   data['Overall_values'] = overallLightIntensity_data
+   data['Threshold_values'] = [HEVthreshold for x in harmfulHEVIntensity_data]
+   labels['Last_Collection_Time'] = [list(df['timeNow'].dt.strftime('%Y-%m-%d'))[-1], time_of_last_collection.strftime('%H:%M')]
+   labels['Label_values'] = list(df['timeNow'].dt.strftime('%H:%M'))
 
+   # Calculate perc of time more than threshold
+   number_of_entries = len(df)
+   number_of_entries_more_than_threshold_HEV = len(df.loc[df.harmfulHEVIntensity > HEVthreshold])
+   number_of_entries_more_than_threshold_Overall = len(df.loc[df.overallLightIntensity > Overallthreshold])
+   data['HEV_overthresh_perc'] = round(number_of_entries_more_than_threshold_HEV/number_of_entries*100)
+   data['Overall_overthresh_perc'] = round(number_of_entries_more_than_threshold_Overall/number_of_entries*100)
+   
+   # Determine overthresh level
+   if data['HEV_overthresh_perc'] > 30:
+      data['HEV_overthresh_level'] = 'High'
+      data['recommendation_text'] = 'Try to reduce electronic device usage and focus on objects about 20 feet away to relax your eyes! At the same time, '
+   else:
+      data['HEV_overthresh_level'] = 'Low'
+      data['recommendation_text'] = 'Blue light levels are low, keep it up! At the same time, '
+   if data['Overall_overthresh_perc'] > 30:
+      data['Overall_overthresh_level'] = 'High'
+      data['recommendation_text'] += 'overall light exposure is high, try to seek shelter!'
+   else:
+      data['Overall_overthresh_level'] = 'Low'
+      data['recommendation_text'] += 'overall light exposure is low, great job!'
    return labels, data
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world_3():
+def disp_graph_user1():
    print(request.method)
    if request.method == 'POST':
       if request.form.get('time_range_button') == 'Last 15 mins':
@@ -57,15 +82,27 @@ def hello_world_3():
       elif request.form.get('time_range_button') == 'Last 24 hours':
          labels_data, values_data = prep_data(60*24,0,0,0)
          time_range_text = '24 hours'
+      elif request.form.get('time_range_button') == 'Last 1 hour':
+         labels_data, values_data = prep_data(60,0,0,0)
+         time_range_text = '1 hour'
 
    elif request.method == 'GET':
       time_range_text = '1 hour'
       labels_data, values_data = prep_data(60,0,0,0)
-   return render_template('hello.html', labels=labels_data, harmful_values=values_data[0], overall_values=values_data[1], HEVthreshold_values = values_data[2], time_range_text = time_range_text)
-
-@app.route('/bla')
-def hello_world_2():
-   return 'hello worldbla'
+   return render_template('summary.html', \
+      labels=labels_data['Label_values'], \
+      harmful_values=values_data['HEV_values'], \
+      overall_values=values_data['Overall_values'], \
+      HEVthreshold_values = values_data['Threshold_values'], \
+      time_range_text = time_range_text, \
+      date = labels_data['Last_Collection_Time'][0], \
+      last_collection_time = labels_data['Last_Collection_Time'][1],
+      bluelight_perc =  values_data['HEV_overthresh_perc'],\
+      overalllight_perc =  values_data['Overall_overthresh_perc'],\
+      bluelight_level = values_data['HEV_overthresh_level'],\
+      overalllight_level = values_data['Overall_overthresh_level'],\
+      recommendation_text = values_data['recommendation_text']
+      )
 
 if __name__ == '__main__':
     app.debug = True
